499974df0a5fc3f7a6747d917da197bfbf22175f
Axel Strömberg <axelstr@users.noreply.github.com>
2024-10-03 13:58:27 -0500

    feat: improve estimation prompt

--- backend/internal/estimation/estimation.go
+++ backend/internal/estimation/estimation.go
@@ -7,8 +7,16 @@ import (
 )
 
 const promptEstimation = `Given the following patient record and provider input,
-estimate the patient's current status. Only use the knowledge database provided
-to inform your estimation.\n
+estimate the patient's current status. The status should contain three sections:
+- Where in the process they are with the provider. For example, are they in the
+initial contact, how many sessions have they been on.
+- The diagnosis of the patient as based on the clinician. If the clinician has't
+made a judment, provide a neutral statement.
+- A 3rd point view of the patient's diagnosis without taking the clinicians
+statement into account.
+Only use the knowledge database provided to inform your estimation. Do not
+provide any suggestions for treatment or action plans as that's provided in
+upcoming steps.\n
 ---
 Patient Record:\n\n%s
 ---
